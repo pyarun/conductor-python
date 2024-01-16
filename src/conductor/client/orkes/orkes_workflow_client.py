90379869a3b60574293b8fe18b0d01945cc0ab59
Viren Baraiya <virenx@gmail.com>
2024-01-16 09:29:18 -0800

    support for older python versions

--- src/conductor/client/orkes/orkes_workflow_client.py
+++ src/conductor/client/orkes/orkes_workflow_client.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 from conductor.client.configuration.configuration import Configuration
 from conductor.client.http.models import SkipTaskRequest, WorkflowStatus, \
@@ -23,7 +23,7 @@ class OrkesWorkflowClient(OrkesBaseClient, WorkflowClient):
     def start_workflow_by_name(
             self,
             name: str,
-            input: dict[str, object],
+            input: Dict[str, object],
             version: Optional[int] = None,
             correlationId: Optional[str] = None,
             priority: Optional[int] = None,
@@ -129,7 +129,7 @@ class OrkesWorkflowClient(OrkesBaseClient, WorkflowClient):
             self,
             batch_request: CorrelationIdsSearchRequest,
             include_completed: bool = False,
-            include_tasks: bool = False) -> dict[str, List[Workflow]]:
+            include_tasks: bool = False) -> Dict[str, List[Workflow]]:
 
         """Given the list of correlation ids and list of workflow names, find and return workflows
         Returns a map with key as correlationId and value as a list of Workflows
@@ -149,7 +149,7 @@ class OrkesWorkflowClient(OrkesBaseClient, WorkflowClient):
             correlation_ids: List[str],
             include_completed: bool = False,
             include_tasks: bool = False
-    ) -> dict[str, List[Workflow]]:
+    ) -> Dict[str, List[Workflow]]:
         """Lists workflows for the given correlation id list"""
         kwargs = {}
         if include_tasks:
@@ -166,5 +166,5 @@ class OrkesWorkflowClient(OrkesBaseClient, WorkflowClient):
     def remove_workflow(self, workflow_id: str):
         self.workflowResourceApi.delete(workflow_id)
 
-    def update_variables(self, workflow_id: str, variables: dict[str, object] = {}) -> None:
+    def update_variables(self, workflow_id: str, variables: Dict[str, object] = {}) -> None:
         self.workflowResourceApi.update_workflow_state(variables, workflow_id)
--- src/conductor/client/workflow_client.py
+++ src/conductor/client/workflow_client.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 from conductor.client.http.models import WorkflowRun, SkipTaskRequest, WorkflowStatus, \
     ScrollableSearchResultWorkflowSummary
@@ -81,7 +81,7 @@ class WorkflowClient(ABC):
             self,
             batch_request: CorrelationIdsSearchRequest,
             include_completed: bool = False,
-            include_tasks: bool = False) -> dict[str, List[Workflow]]:
+            include_tasks: bool = False) -> Dict[str, List[Workflow]]:
         pass
 
     @abstractmethod
@@ -91,7 +91,7 @@ class WorkflowClient(ABC):
             correlation_ids: List[str],
             include_completed: bool = False,
             include_tasks: bool = False
-    ) -> dict[str, List[Workflow]]:
+    ) -> Dict[str, List[Workflow]]:
         pass
 
     @abstractmethod
@@ -99,5 +99,5 @@ class WorkflowClient(ABC):
         pass
 
     @abstractmethod
-    def update_variables(self, workflow_id: str, variables: dict[str, object] = {}) -> None:
+    def update_variables(self, workflow_id: str, variables: Dict[str, object] = {}) -> None:
         pass
