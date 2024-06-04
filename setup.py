96680c83016a917aeae66d152c488ad7db98c15b
Arun Mittal <mittal.talk@gmail.com>
2024-06-04 10:26:28 +0530

    changed CONDUCTOR_PYTHON_VERSION syntax

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import os
 
-version = os.environ['CONDUCTOR_PYTHON_VERSION']
+version = os.environ.get('CONDUCTOR_PYTHON_VERSION', "1.1.7")
 if version is None:
     version = '0.0.0-SNAPSHOT'
 
